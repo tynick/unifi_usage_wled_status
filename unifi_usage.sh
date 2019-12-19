050e345c08cadaced2e77a146eeae550c821ff41
tynick <ballerstat@gmail.com>
2019-12-18 18:28:02 -0700

    using this because we can reset it without a reboot or kernel reload

--- unifi_usage.sh
+++ unifi_usage.sh
@@ -7,7 +7,7 @@
 interface="eth2"
 
 # wled IP address
-wled_ip='192.168.1.145'
+wled_ip='192.168.1.153'
 
 # status colors
 very_bad='&A=128&FX=1&SX=245&R=255&G=0&B=0'
@@ -18,15 +18,19 @@ good='&A=128&FX=0&SX=0&R=127&G=255&B=0'
 great='&A=128&FX=0&SX=0&R=0&G=255&B=0'
 
 # get total bytes from WAN interface
-rx_bytes_used=$(cat /sys/class/net/"${interface}"/statistics/rx_bytes | awk '{ gb =$1 /1024/1024/1024; print gb }')
-tx_bytes_used=$(cat /sys/class/net/"${interface}"/statistics/tx_bytes | awk '{ gb =$1 /1024/1024/1024; print gb }')
+rx_bytes_used=$(/opt/vyatta/bin/vyatta-op-cmd-wrapper show interfaces ethernet "${interface}" | grep 'RX' -A1 | tail -1 | awk '{ print $1 }' | awk '{ gb =$1 /1024/1024/1024; print gb }')
+tx_bytes_used=$(/opt/vyatta/bin/vyatta-op-cmd-wrapper show interfaces ethernet "${interface}" | grep 'TX' -A1 | tail -1 | awk '{ print $1 }' | awk '{ gb =$1 /1024/1024/1024; print gb }')
 
 # convert bytes to gigabytes. remove decimal and "round up" by 1
 rx_gb_used=$(echo "${rx_bytes_used}" | cut -d \. -f 1); let rx_gb_used++
 tx_gb_used=$(echo "${tx_bytes_used}" | cut -d \. -f 1); let tx_gb_used++
 let "total_gb_used=rx_gb_used+tx_gb_used"
 
-echo "${total_gb_used}"
+echo "rx bytes = "${rx_bytes_used}""
+echo "tx bytes = "${tx_bytes_used}""
+echo "rx gb = "${rx_gb_used}""
+echo "tx gb = "${tx_gb_used}""
+echo "total gb = "${total_gb_used}""
 
 # set wled params based on total_gb_used
 if [[ "${total_gb_used}" -gt 999 ]]; then

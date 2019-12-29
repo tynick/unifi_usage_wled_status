d0d567444d2e515edf57d946e4967d5e561ee438
tynick <ballerstat@gmail.com>
2019-12-29 11:36:55 -0700

    change link to directions and new port info for regular USG

--- reset_usage.sh
+++ reset_usage.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# tynick.com
+# tynick.com/blog/12-29-2019/internet-data-usage-monitoring-so-you-dont-hit-your-data-cap/
 # reset interface counter on ubiquiti USG port
 
 # USG port
-interface="eth2"
+# uncomment/comment the next two lines based on what USG model you have
+interface="eth2" # USG PRO 4
+#interface="eth0" # USG
 
 # clear interface counters
 /opt/vyatta/bin/vyatta-op-cmd-wrapper clear interfaces ethernet "${interface}" counters
